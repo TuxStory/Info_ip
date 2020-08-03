658fded18cca8fb0977306f7e45cad3eefdaaa7f
TuxStory <ajm.even@gmail.com>
2020-08-03 22:24:16 +0200

    Ajout du script version 0.3

---
+++ info_ip.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#################################
+# Nom		: 		#
+# Auteur 	: Antoine Even	#
+# Date 		: 07/06/20	#
+# Version	: 0.0.3		#
+#################################
+
+#variables
+GREEN='\033[0;32m'
+WHITE='\033[1;37m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+
+IP=$(hostname -I | awk '{print $1}')
+IP_PUB=$(curl ifconfig.me)
+NAME=$(hostname)
+TEMPS=$(uptime -p | awk '{for(i=2;i<=NF;++i)print $i}')
+#CP =$(lscpu | grep  @ | awk '{print $4,$NF}' Essai avec awk)
+CPU=$(cat /proc/cpuinfo | grep -i "^model name" | awk -F ": " '{print $2}' | head -1 | sed 's/ \+/ /g')
+MEM=$(free -h | grep Mem | awk '{print $2}')
+KERNEL=$(uname -a | awk '{print $3}')
+#DISQUE=$(df -h | grep sd)
+GPU=$(lspci | grep VGA | awk '{for(i=5;i<=NF;++i)print $i}')
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+    ID=$ID
+fi
+
+#Programme
+clear
+echo "==================== IP ===================="
+echo -e ${WHITE}"IP locale         : "${GREEN}$IP
+echo -e ${WHITE}"IP Publique       : "${GREEN}$IP_PUB
+echo -e ${WHITE}"=================== Infos =================="
+echo -e ${WHITE}"Distribution 	  : "$OS
+echo "Version		  : "$VER
+echo -e "Version du Noyau  : "${BLUE}$KERNEL
+echo -e ${WHITE}"Hostname    	  : "$HOSTNAME
+echo "En Fonction 	  : "$TEMPS
+echo "Environement	  : "$DESKTOP_SESSION
+echo "=================== Matériel =================="
+echo "Processeur : "$CPU
+echo "Mémoire Ram : "$MEM
+echo "Carte Graphique : "$GPU
+echo "Disques : " ; df -h | grep sd
+echo "================== Température =================="
+if [ $ID == "fedora" ]; then
+	echo "Temperature disques :" ; hddtemp
+elif [ $ID == "debian" ]; then
+	echo "Temperature disques :" ; hddtemps /dev/sd*
+else
+	echo "Temperature disques :" ; hddtemps /dev/sd*
+fi
+
+#Temperature
+if [[ -x "/usr/bin/sensors" ]]; then
+    sensors
+fi
